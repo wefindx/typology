ed45b2ba3d6d0855f3b71864b40724c8897ed276
Mindey <mindey@qq.com>
2018-06-23 23:27:21 +0000

    +changes

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name='typology',
-    version='0.0.2',
+    version='0.0.3',
     description='Python formats derived from vocabulary concepts.',
-    url='https://github.com/mindey/typology',
+    url='https://gitlab.com/wefindx/typology',
     author='Mindey',
     author_email='mindey@qq.com',
-    license='AGPL',
+    license='ASK FOR PERMISSIONS',
     packages = find_packages(exclude=['docs', 'tests*']),
     install_requires=['requests'],
     extras_require = {
